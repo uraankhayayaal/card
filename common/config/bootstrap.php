9acc58893eb79d9f9b9c23cd3586d9eba3d6518e
Ilya <teterin.i.n.job@gmail.com>
2016-10-10 15:49:57 +0900

    bag fix

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -5,4 +5,4 @@ Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@cabinet', dirname(dirname(__DIR__)) . '/cabinet');
 Yii::setAlias('@oauth', dirname(dirname(__DIR__)) . '/oauth');
-Yii::setAlias('@frontend-web', 'http://card.dty.local/');
+Yii::setAlias('@frontend-web', 'http://card.dty.su/');
