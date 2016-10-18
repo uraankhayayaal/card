c17d32524ee4b6b750ea4b3eb1b29dfd6874566a
devdty <admin@dty.su>
2016-10-18 07:49:18 +0000

    fix

--- oauth/controllers/OauthController.php
+++ oauth/controllers/OauthController.php
@@ -32,6 +32,7 @@ class OauthController extends Controller
      */
     public function actionAuthorize($client_id, $redirect_uri, $id)
     {
+	$this->log("Authorize...");
         if (OauthClient::findOne(['client_id' => $client_id])) {
             if (!Yii::$app->user->isGuest) {
                 return $this->redirect(['access', 'client_id' => $client_id, 'redirect_uri' => $redirect_uri, 'id' => $id]);
@@ -51,8 +52,11 @@ class OauthController extends Controller
 
     public function actionAccess($client_id, $redirect_uri, $id)
     {
+	$this->log("Access");
         if (OauthClient::findOne(['client_id' => $client_id])) {
+		$this->log("Access - One if");
             if ($model = OauthAccessToken::find()->where(['client_id' => $client_id, 'user_id' => Yii::$app->user->identity->id])->one()) {
+		$this->log("Access - Two if");
                 $url = "{$redirect_uri}?client_id={$client_id}&redirect_uri={$redirect_uri}&code={$model->user_id}&id={$id}&status_code=200";
                 return $this->redirect(Url::to($url));
             }
@@ -91,7 +95,7 @@ class OauthController extends Controller
 
     public function actionGenerateAccessToken($client_id, $client_secret, $code, $redirect_uri, $id)
     {
-        $this->log('0');
+        $this->log("GAT");
         if (OauthClient::find()->where(['client_secret' => $client_secret, 'client_id' => $client_id])->one())
         {
             $this->log('1');
